ec3011cbc43435770d9afddce52de4c06851eac5
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-04-02 01:23:39 +0600

    Create database connection function

---
+++ src/db/connectDb.js
@@ -0,0 +1,23 @@
+// import
+import mongoose from 'mongoose';
+
+const connection = {};
+
+const connectDb = async () => {
+   try {
+      if (connection?.isConnected) {
+         return;
+      }
+
+      const clientOptions = {
+         serverApi: { version: '1', strict: true, deprecationErrors: true },
+      };
+
+      await mongoose.connect(process.env.DB_URI, clientOptions);
+      connection.isConnected = true;
+   } catch (error) {
+      throw new Error(error.message);
+   }
+};
+
+export default connectDb;
